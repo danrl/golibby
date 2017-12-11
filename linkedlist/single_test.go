2f6204c17d16ec5ef28afce6bccea5033eddc6ae
Dan Lüdtke <dan.luedtke@egym.de>
2017-12-11 14:45:34 +0100

    added single linked list

---
+++ linkedlist/single_test.go
@@ -0,0 +1,104 @@
+package linkedlist
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	ll := NewSingle()
+	assert.NotEqual(t, nil, ll)
+}
+
+func TestAppend(t *testing.T) {
+	ll := NewSingle()
+	ll.Append("foo")
+	assert.NotEqual(t, nil, ll.head)
+	assert.Equal(t, "foo", ll.head.val)
+
+	ll.Append("bar")
+	assert.NotEqual(t, nil, ll.head.next)
+	assert.Equal(t, "bar", ll.head.next.val)
+
+	ll.Append("lorem")
+	assert.NotEqual(t, nil, ll.head.next.next)
+	assert.Equal(t, "lorem", ll.head.next.next.val)
+}
+
+func TestRemove(t *testing.T) {
+	// beginning
+	{
+		ll := NewSingle()
+		ll.Append("foo")
+		ll.Append("bar")
+		ll.Append("lorem")
+
+		ll.Remove("foo")
+		assert.Equal(t, "bar", ll.head.val)
+		assert.NotEqual(t, nil, ll.head.next)
+	}
+	// middle
+	{
+		ll := NewSingle()
+		ll.Append("foo")
+		ll.Append("bar")
+		ll.Append("lorem")
+
+		ll.Remove("bar")
+		assert.Equal(t, "foo", ll.head.val)
+		assert.NotEqual(t, nil, ll.head.next)
+		assert.Equal(t, "lorem", ll.head.next.val)
+		assert.Equal(t, (*item)(nil), ll.head.next.next)
+	}
+	// end
+	{
+		ll := NewSingle()
+		ll.Append("foo")
+		ll.Append("bar")
+		ll.Append("lorem")
+
+		ll.Remove("lorem")
+		assert.Equal(t, (*item)(nil), ll.head.next.next)
+	}
+	// not found
+	{
+		ll := NewSingle()
+		ll.Append("foo")
+		ll.Append("bar")
+
+		err := ll.Remove("lorem")
+		assert.Equal(t, ErrorNotFound, err)
+	}
+}
+
+func TestIter(t *testing.T) {
+	ll := NewSingle()
+	ll.Append("foo")
+	ll.Append("bar")
+	ll.Append("lorem")
+	var i int
+	for val := range ll.Iter() {
+		switch i {
+		case 0:
+			assert.Equal(t, "foo", val)
+		case 1:
+			assert.Equal(t, "bar", val)
+		case 2:
+			assert.Equal(t, "lorem", val)
+		}
+		i++
+	}
+}
+
+func TestLen(t *testing.T) {
+	ll := NewSingle()
+	assert.Equal(t, 0, ll.Len())
+
+	ll.Append("foo")
+	assert.Equal(t, 1, ll.Len())
+	ll.Append("bar")
+	assert.Equal(t, 2, ll.Len())
+	_ = ll.Remove("foo")
+	assert.Equal(t, 1, ll.Len())
+}
