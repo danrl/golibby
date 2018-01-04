c12e96cf5dd81f4d2a0733cd787f1760bfc00e9f
Dan Lüdtke <mail@danrl.com>
2018-01-04 20:28:41 +0100

    concurrency safe: linkedlist

--- queue/queue.go
+++ queue/queue.go
@@ -1,10 +1,14 @@
 package queue
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // Queue represents a queue
 type Queue struct {
 	data []interface{}
+	lock sync.RWMutex
 }
 
 var (
@@ -19,16 +23,22 @@ func New() *Queue {
 
 // Len returns the number of items in the queue
 func (q *Queue) Len() int {
+	q.lock.RLock()
+	defer q.lock.RUnlock()
 	return len(q.data)
 }
 
 // Add adds an item at the end of the queue
 func (q *Queue) Add(item interface{}) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
 	q.data = append(q.data, item)
 }
 
 // Peek returns the first item from the queue without removing it
 func (q *Queue) Peek() (interface{}, error) {
+	q.lock.RLock()
+	defer q.lock.RUnlock()
 	if len(q.data) == 0 {
 		return nil, ErrorEmpty
 	}
@@ -37,6 +47,8 @@ func (q *Queue) Peek() (interface{}, error) {
 
 // Remove returns the first item from the queue
 func (q *Queue) Remove() (interface{}, error) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
 	if len(q.data) == 0 {
 		return nil, ErrorEmpty
 	}
