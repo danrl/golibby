9c03ff28da8dbee79dbc7f476dde942fbd9d4b55
Dan Lüdtke <mail@danrl.com>
2018-01-12 21:38:14 +0100

    utils: variadic signature for Min() and Max()

--- utils/minmax_test.go
+++ utils/minmax_test.go
@@ -9,9 +9,11 @@ import (
 func TestMin(t *testing.T) {
 	assert.Equal(t, 1, Min(1, 2))
 	assert.Equal(t, 1, Min(2, 1))
+	assert.Equal(t, 1, Min(2, 1, 3))
 }
 
 func TestMax(t *testing.T) {
 	assert.Equal(t, 2, Max(1, 2))
 	assert.Equal(t, 2, Max(2, 1))
+	assert.Equal(t, 2, Max(0, 2, 1))
 }
