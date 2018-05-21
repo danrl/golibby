777b3647499c7ae8996c5a53cd35a597e0b62d38
Dan Lüdtke <mail@danrl.com>
2018-05-21 11:28:24 +0200

    metric/popcount: switched to Brian Kernighan algorithm

--- metric/popcount.go
+++ metric/popcount.go
@@ -1,22 +1,21 @@
 package metric
 
-// PopulationCount returns the bit sum of a byte
+// PopulationCount returns the bit sum of a byte using Brian Kernighan's
+// algorithm with a time complexity of O(log n)
 func PopulationCount(x byte) int {
-	s := 0
-	for x > 0 {
-		if x&0x1 == 1 {
-			s++
-		}
-		x = x >> 1
+	count := 0
+	for x != 0 {
+		count++
+		x &= (x - 1)
 	}
-	return s
+	return count
 }
 
 // PopulationCounts returns the bit sum of all bytes in a byte slice
 func PopulationCounts(xs []byte) int {
-	s := 0
+	count := 0
 	for _, x := range xs {
-		s += PopulationCount(x)
+		count += PopulationCount(x)
 	}
-	return s
+	return count
 }
